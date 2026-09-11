b63502f9f4c459db7163424ad887b7055ad2851e
Adam <quocan1101@gmail.com>
2026-09-11 23:30:07 +0800

    Stock as integer

--- auditor.py
+++ auditor.py
@@ -4,7 +4,7 @@ while True:
     input_value = input("Enter the number of items to add to inventory (or type 'quit' to exit): ")
     if input_value == "quit":
         break
-    val = int(input_value)
+    val = int(input_value) #Accept stock as integer
     inventory += val
 
 print(f"Total inventory: {inventory}")
